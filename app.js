ff38d7292a64e23d641b6cb10e0eb7cb6ab2e3a3
Emily Aguirre <emgal.aguirre@gmail.com>
2020-09-30 13:18:21 -0500

    all cards working

--- app.js
+++ app.js
@@ -1,129 +1,5 @@
-// const Manager = require("./lib/Manager");
-
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
-
-// const team = [];
-
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-// const render = require("./lib/htmlRenderer");
-
-// function employeeInfo() {
-//     inquirer.prompt([
-//         {
-//             type: "list",
-//             message: "What type of employee would you like to add?",
-//             name: "name",
-//             choices: ["Intern", "Engineer", "Manager", "Team Complete"],
-//         },
-//     ]).then(val => {
-//         if (val.name === "Intern") {
-//             internInfo();
-//         } else if (val.name === "Engineer") {
-//             engineerInfo();
-//         } else if (val.name === "Manager") {
-//             managerInfo();
-//         } else if (val.name === "Team Complete") {
-//             generateHTML(outputPath, render(team));
-//         };
-//     });
-// };
-
-// function managerInfo() {
-//     return inquirer.prompt([
-//         {
-//             message: "What is the manager's name?",
-//             name: "name"
-//         },
-//         {
-//             message: "What is the manager's id?",
-//             name: "id"
-//         },
-//         {
-//             message: "What is the manager's email?",
-//             name: "email"
-//         },
-//         {
-//             message: "What is the manager's ofice number?",
-//             name: "officeNumber"
-//         },
-//     ]).then(function (answer) {
-//         let manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber)
-//         team.push(manager);
-
-//         employeeInfo()
-//     })
-// };
-
-// function engineerInfo() {
-//     return inquirer.prompt([
-//         {
-//             message: "What is the engineer's name?",
-//             name: "name"
-//         },
-//         {
-//             message: "What is the engineer's id?",
-//             name: "id"
-//         },
-//         {
-//             message: "What is the engineer's email?",
-//             name: "email"
-//         },
-//         {
-//             message: "What is the engineer's GitHub username?",
-//             name: "github"
-//         },
-//     ]).then(function (answer) {
-//         let engineer = new Engineer(answer.name, answer.id, answer.email, answer.github)
-//         team.push(engineer);
-
-//         employeeInfo()
-//     })
-// };
-
-// function internInfo() {
-//     return inquirer.prompt([
-//         {
-//             message: "What is the intern's name?",
-//             name: "name"
-//         },
-//         {
-//             message: "What is the intern's id?",
-//             name: "id"
-//         },
-//         {
-//             message: "What is the intern's email?",
-//             name: "email"
-//         },
-//         {
-//             message: "What school does the intern go to?",
-//             name: "school"
-//         },
-//     ]).then(function (answer) {
-//         let engineer = new Engineer(answer.name, answer.id, answer.email, answer.school)
-//         team.push(intern);
-
-//         employeeInfo()
-//     })
-// };
-
-// function generateHTML(fileName, data) {
-//     fs.writeFile(fileName, data, "utf8", function (err) {
-//         if (err) {
-//             throw err;
-//         }
-//         console.log("Perfect! Your team info is now complete!");
-//     });
-// };
-
-// employeeInfo();
-
 const Manager = require("./lib/Manager");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
@@ -131,7 +7,6 @@ const path = require("path");
 const fs = require("fs");
 
 const team = [];
-// const stringArr = JSON.stringify(team);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -142,7 +17,7 @@ function employeeInfo() {
     inquirer.prompt([
         {
             type: "list",
-            message: "What type of employee would you like add?",
+            message: "What type of employee would you like to add?",
             name: "name",
             choices: ["Intern", "Engineer", "Manager", "Team Complete"],
         },
@@ -174,7 +49,7 @@ function managerInfo() {
             name: "email"
         },
         {
-            message: "What is the manager's office number?",
+            message: "What is the manager's ofice number?",
             name: "officeNumber"
         },
     ]).then(function (answer) {
@@ -200,14 +75,14 @@ function engineerInfo() {
             name: "email"
         },
         {
-            message: "What is the engineer's Github username?",
+            message: "What is the engineer's GitHub username?",
             name: "github"
-        }
+        },
     ]).then(function (answer) {
         let engineer = new Engineer(answer.name, answer.id, answer.email, answer.github)
         team.push(engineer);
 
-        employeeInfo();
+        employeeInfo()
     })
 };
 
@@ -228,12 +103,12 @@ function internInfo() {
         {
             message: "What school does the intern go to?",
             name: "school"
-        }
+        },
     ]).then(function (answer) {
         let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
         team.push(intern);
 
-        employeeInfo();
+        employeeInfo()
     })
 };
 
@@ -242,7 +117,7 @@ function generateHTML(fileName, data) {
         if (err) {
             throw err;
         }
-        console.log("All done! Your team info is now complete!");
+        console.log("Perfect! Your team info is now complete!");
     });
 };
 
